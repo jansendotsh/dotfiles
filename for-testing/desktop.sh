623712b0e91b3e89bcc6034f11ac8bec8b8bfd5c
Garrett Jansen <garrett@jansen.sh>
2018-11-05 21:37:19 -0500

    Testing provisioner script

--- for-testing/desktop.sh
+++ for-testing/desktop.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/sh 
 
-dnf makecache
+sudo dnf -y update
+
 sudo dnf -y groupinstall gnome
-sudo dnf -y install gdm
+sudo dnf -y install gdm gcc kernel-devel kernel-headers dkms make bzip2 perl
 sudo systemctl enable gdm
 sudo systemctl start gdm
 
