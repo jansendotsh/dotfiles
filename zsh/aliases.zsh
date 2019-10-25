63925ac7fc22b47fd3b22dfac1374bf624986e1c
garrettjj <garrett@jansen.sh>
2019-10-24 23:30:08 -0400

    Minor updates to fix vim packages

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,10 +6,7 @@ alias ssh-start='eval $(ssh-agent); ssh-add -k ~/.ssh/id_rsa'
 alias ll='ls -larth'
 
 # Work aliases
-alias proxy='ssh -D 54322 janseng@ipmon25.mastercard'
-alias proxy2='ssh -D 54322 janseng@ipmon20.mastercard'
-alias devstg='ssh -D 54321 janseng@ipmon01.mastercard'
-alias tunnel='ssh $1 -l e082561 -p 2222 -o "ProxyCommand=nc -X 5 -x 127.0.0.1:54322 %h %p"'
+# Temporarily empty :)
 
 # Python settings
 alias python='python3'
