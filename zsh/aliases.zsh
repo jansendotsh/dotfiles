cc4ef11e947458188ddc1da667e877e57280833e
jansendotsh <garrett@jansen.sh>
2021-02-25 13:10:46 -0800

    Including TPM, alias updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,7 +6,11 @@ alias ssh-start='eval $(ssh-agent); ssh-add -k ~/.ssh/id_rsa'
 alias ll='ls -larth'
 
 # Work aliases
-# Temporarily empty :)
+alias nyc3vpn='sudo openconnect --verbose --verbose --timestamp --dump-http-traffic --protocol=gp --csd-wrapper=/usr/libexec/openconnect/hipreport.sh https://vpn-nyc3.digitalocean.com/ssl-vpn' 
+alias sfo2vpn='sudo openconnect --verbose --verbose --timestamp --dump-http-traffic --protocol=gp --csd-wrapper=/usr/libexec/openconnect/hipreport.sh https://vpn-sfo2.digitalocean.com/ssl-vpn'
+
+# Personal logins
+alias oplogin='eval $(op signin gjansen)'
 
 # Python settings
 alias python='python3'
@@ -23,9 +27,13 @@ alias tdroplets='doctl --context team compute droplet list --format "ID,Name,Pub
 alias pdoctl='doctl --context personal'
 alias wdoctl='doctl --context work'
 alias tdoctl='doctl --context team'
+source  <(doctl completion zsh)
 
 # LaTeX
 alias apatex='cp $HOME/Git/LaTeX-APA/apa.tex $1'
 
 # Todoist CLI
 alias todoist='todoist --color'
+
+# FuzzyFinder
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
